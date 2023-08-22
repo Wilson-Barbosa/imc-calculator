34d4240625331b0b55e7a56a56a8c367ac058d42
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2023-08-22 17:34:07 -0300

    adding more code to the application

---
+++ src/app/components/calculator/calculator.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent {
+
+    //attributes
+    //i had to add this ! aftet their names, apparently in this version of typescript all the properties should be initialized within the constructor (i'm not using one here)
+    weight!: number;
+    height!: number;
+    imcIndex!: number;
+
+    //function to calculate imc
+    imcCalculate(){
+      this.imcIndex = this.weight / this.height;
+    }
+}
