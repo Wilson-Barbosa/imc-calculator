43f53501c1a8028a4561d046feb33c0ba2d2e52c
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2023-08-28 21:05:05 -0300

    more code to finish the project

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -8,17 +8,29 @@ import { Component } from '@angular/core';
 export class CalculatorComponent {
 
     //attributes
-    //i had to add this ! aftet their names, apparently in this version of typescript all the properties should be initialized within the constructor (i'm not using one here)
-    weight!: number;
-    height!: number;
-    imcIndex!: number;
+    //i had to change an option on tsconfig, apparently newer versions of ts demand all properties to be declared before used (or something like it)
+    weight: number;
+    height: number;
+    imc: number;
+    answer: string;
 
     //function to calculate imc
     imcCalculate(){
-        this.imcIndex = this.weight / this.height;
+        this.imc = this.weight / (this.height * this.height);
+
+        if (this.imc < 18.5) {
+            this.answer = 'Magreza';
+        } else if (this.imc >= 18.5 && this.imc < 25) {
+            this.answer = 'Peso Normal';
+        } else if (this.imc >= 25 && this.imc < 30) {
+            this.answer = 'Sobrepeso';
+        } else if (this.imc >= 30 && this.imc < 35) {
+            this.answer = 'Obesidade grau I';
+        } else if (this.imc >= 35 && this.imc <= 40) {
+            this.answer = 'Obesidade grau II';
+        } else if (this.imc > 40) {
+            this.answer = 'Obesidade grau III';
+        }
     }
 
-    imcRating() {
-        
-    }
 }
